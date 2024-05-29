2a80cb1219977c8d40dbb01d2162fbd67a55c48f
thulanimtetwa <thulanimtetwa@gmail.com>
2024-05-29 07:15:15 +0200

    Initial Commit

---
+++ FormatterHelper/FormatterHelper.h
@@ -0,0 +1,18 @@
+//
+//  FormatterHelper.h
+//  FormatterHelper
+//
+//  Created by Thulani Mtetwa on 2024/05/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FormatterHelper.
+FOUNDATION_EXPORT double FormatterHelperVersionNumber;
+
+//! Project version string for FormatterHelper.
+FOUNDATION_EXPORT const unsigned char FormatterHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FormatterHelper/PublicHeader.h>
+
+
